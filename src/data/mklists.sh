9958fbff7c46b40d23d76fb0311f9f4cd41c1877
Diomidis Spinellis <dds@aueb.gr>
2022-01-23 14:44:24 +0200

    Add Ancient Greek words and creation script

--- src/data/mklists.sh
+++ src/data/mklists.sh
@@ -4,35 +4,46 @@
 # Diomidis Spinellis, January 2022
 #
 
-# Date of the Wikipedia dump to fetch
-DUMPDATE=20220101
+if ! [ -d Classics.nbk ] ; then
+  https://www.perseus.tufts.edu/hopper/opensource/downloads/texts/hopper-texts-GreekRoman.tar.gz |
+    tar xzf -
+  mv Classics Classics.nbk
+fi
 
-# Normalize a list of words to lowercase 5-character non-stress words
-normalize()
-{
-  sed '
-  # Remove lowercase stresses
-  y/άέήίϊΐόύϋΰώς/αεηιιιουυυωσ/
-  # Remove uppercase stresses
-  y/ΆΈΉΊΪΌΎΫΏ/ΑΕΗΙΙΟΥΥΩ/
-  # Map lowercase to uppercase
-  y/αβγδεζηθικλμνξοπρστυφχψω/ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ/
-  # One Greek word per line; remove all else
-  s/[^ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ]/\n/g
-  ' |
+find Classics.nbk -name \*_gk.xml |
+  grep -v HistAugust |
+  while read f ; do
+    sed '1,/<text[^>]*>/d
+      # Remove e.g. Latin text blocks
+      /<foreign[^>]*>[^<]*$/,/<\/foreign/d
+      # Remove e.g. Latin text in lines
+      s/<foreign[^>]*>[^<]*//
+      # Remove notes
+      s/<note>.*<\/note>//g;
+      # Remove other tags
+      s/<[^>]*>//g
+      # Remove entities
+      s/&[^;]*;//g
+      # Remove stresses
+      s/[|/\\=()+]//g
+      # Convert uppercase to Greek
+      y/ABCDEFGHIKLMNOPQRSTUWXYZ/ΑΒΞΔΕΦΓΗΙΚΛΜΝΟΠΘΡΣΤΥΩΧΨΖ/
+      # Convert lowercase to Greek
+      y/abcdefghiklmnopqrstuwxyz/ΑΒΞΔΕΦΓΗΙΚΛΜΝΟΠΘΡΣΤΥΩΧΨΖ/
+      # Remove words with omitted characters
+      s/[ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ]*'\''//g
+      # Remove other signs
+      s/[*"]//g
+      # One Greek word per line; remove all else
+      s/[^ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ]/\n/g
+      ' "$f"
+  done |
   # Keep only 5-character Greek words
-  egrep '^[ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ]{5}$'
-}
-
-# Create list of allowed words
-
-# Convert the hunspell-el dictionary to UTF-8
-iconv -f ISO-8859-7 -t UTF-8 /usr/share/myspell/el_GR.dic |
-  # Remove proper nouns
-  grep -v '^[Α-ΩΆΈΉΊΌΎΏ]' |
-  normalize |
-  # Keep for later use
-  sort -u >allowed-words.txt
+  egrep '^[ΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ]{5}$' |
+  sort |
+  uniq -c |
+  sort -rn |
+  awk '{print $2}' >all-words.txt
 
 # Create JavaScript allowed word list
 # Convert to JavaScript object
@@ -44,24 +55,11 @@ $a\
 }\
 \
 export default words
-' <allowed-words.txt > words.js
+' <all-words.txt > words.js
 
-# Create possible answers from frequent correct Wikipedia words
-
-# Obtain Greek Wikipedia dump
-curl https://ftp.acc.umu.se/mirror/wikimedia.org/dumps/elwiki/$DUMPDATE/elwiki-$DUMPDATE-pages-articles.xml.bz2 |
-  # Decompress
-  bzip2 -dc |
-  normalize |
-  sort |
-  # Count occurrences
-  uniq -c |
-  # Get only correct ones
-  join -2 2 allowed-words.txt - |
-  # Order by frequency
-  sort -k2rn |
-  # Get most frequent ones (same number as English)
-  head -n 2079 |
+# Create possible answers from frequent correct words
+# Get most frequent ones (same number as English)
+head -n 2080 all-words.txt |
   sort |
   # Convert to JavaScript array
   sed '1i\
@@ -76,4 +74,4 @@ $a\
 export default answers
 ' >answers.js
 
-rm allowed-words.txt
+rm all-words.txt
